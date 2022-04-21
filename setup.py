59a220a54049ec10e7cc97b79afc6d2090e21026
gwbres <guillaume.bressaix@gmail.com>
2022-04-21 15:00:24 +0200

    setup script

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(name="distutils",
     version="1.0",
-    description="Analog Devices Clock synthesizers (AD95xx) management tools",
+    description="Analog Devices (AD9545,46) management tools",
     author="Guillaume W. Bres",
     author_email="guillaume.bressaix@gmail.com",
-    scripts=["profile.py"],
+    scripts=["profile.py","status.py"],
 )
