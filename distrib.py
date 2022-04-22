16d22d26e8338e05013e83850df7813091a39516
gwbres <guillaume.bressaix@gmail.com>
2022-04-22 18:08:32 +0200

    working on distrib script

--- distrib.py
+++ distrib.py
@@ -47,12 +47,14 @@ def main (argv):
         help="Select which channel to configure. Defaults to `all`. Refer to README",
     )
     flags = [
-        ("sync-all", None, """Synchronize all distribution dividers. 
+        ("sync-all", None, [], """Synchronize all distribution dividers. 
         If output behavior is not set to `immediate`, one must run a `sync-all` to 
         output a synthesis."""),
-        ("divide-ratio", int, "A/AA/B/BB/C/CC integer paths div. ratio."), 
+        ("divide-ratio", int, [], "A/AA/B/BB/C/CC integer paths div. ratio."), 
+        ('auto-sync', str, ['manual', 'immediate','phase','freq'], 'Set output pin behavior. Refer to README'),
+        ('q-sync', None, [], 'Initialize a sync sequence on Q div. stage manually. Refer to README'),
     ]
-    for (v_label, v_type, v_helper) in flags:
+    for (v_label, v_type, v_choices, v_helper) in flags:
         if v_type is None:
             parser.add_argument(
                 "--{}".format(v_label), 
@@ -62,6 +64,7 @@ def main (argv):
         else:
             parser.add_argument(
                 "--{}".format(v_label), 
+                choices=v_choices,
                 type=v_type,
                 help=v_helper,
             )
@@ -74,11 +77,45 @@ def main (argv):
 
     # Special Flags
     if args.sync_all:
-        reg = read_data(handle, address, 0x2000)
-        write_data(handle, address, 0x2000, reg|0x08)
+        write_data(handle, address, 0x2000, 0x08)
         write_data(handle, address, 0x000F, 0x01) # IO update
-        write_data(handle, address, 0x2000, reg&0xF7)
-        write_data(handle, address, 0x000F, 0x01) # IO update
+
+    core = args.core
+    channel = args.channel
+    if args.auto_sync:
+        value = 0x00
+        if args.auto_sync == 'immediate':
+            value = 0x01
+        if args.auto_sync == 'phase':
+            value = 0x02
+        if args.auto_sync == 'freq':
+            value = 0x03
+        if args.channel == '0':
+            reg = read_data(handle, address, 0x10DB)
+            write_data(handle, address, 0x10DB, reg|value)
+        elif args.channel == '1':
+            reg = read_data(handle, address, 0x14DB)
+            write_data(handle, address, 0x14DB, reg|value)
+        else: # all
+            reg = read_data(handle, address, 0x10DB)
+            write_data(handle, address, 0x10DB, reg|value)
+            write_data(handle, address, 0x10DB, reg|value)
+            reg = read_data(handle, address, 0x14DB)
+            write_data(handle, address, 0x14DB, reg|value)
+            write_data(handle, address, 0x14DB, reg|value)
+    
+    if args.q_sync:
+        if args.channel == '0':
+            reg = read_data(handle, address, 0x2101)
+            write_data(handle, address, 0x2101, reg|0x08)
+        elif args.channel == '1':
+            reg = read_data(handle, address, 0x2201)
+            write_data(handle, address, 0x2201, reg|0x08)
+        else: # all
+            reg = read_data(handle, address, 0x2101)
+            write_data(handle, address, 0x2101, reg|0x08)
+            reg = read_data(handle, address, 0x2201)
+            write_data(handle, address, 0x2201, reg|0x08)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
