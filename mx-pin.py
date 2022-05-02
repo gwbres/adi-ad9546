20fc07e6fb66cfa6040c686ea669e1973455a762
gwbres <guillaume.bressaix@gmail.com>
2022-05-02 09:52:37 +0200

    mx-pin script

--- mx-pin.py
+++ mx-pin.py
@@ -30,10 +30,17 @@ def main (argv):
         "address",
         help="I2C slv address",
     )
+    #parser.add_argument(
+    #    "op",
+    #    metavar="operation",
+    #    choices=["config","assert"],
+    #    type=str,
+    #    help="Select whether we are configure or asserting a previously configured pin",
+    #)
     parser.add_argument(
         "pin",
         metavar="pin",
-        choices=["M1","M2","M3","M4","M5","M6"],
+        choices=["M0", "M1","M2","M3","M4","M5","M6"],
         help="Select with pin to assign",
         type=str,
     )
@@ -41,11 +48,76 @@ def main (argv):
         "mode",
         metavar="mode",
         choices=["control","status"],
-        help="Select whether this is a control or a status assignment",
+        help="Select whether this is a TX or an RX pin",
         type=str,
     )
+    parser.add_argument(
+        "--drv",
+        metavar="drive",
+        choices=["normal","inverted","open-drain","open-drain-inv"],
+        type=str,
+        help="Select TX logic",
+    )
+    parser.add_argument(
+        "--rcv",
+        metavar="receive",
+        choices=["and", "nand", "or", "nor"],
+        type=str,
+        help="Select RX logic",
+    )
+    parser.add_argument(
+        '--current',
+        metavar='current',
+        choices=['3mA','6mA'],
+        default='6mA',
+        type=str,
+        help="Ouptut current (drive strength); default is 6mA",
+    )
     args = parser.parse_args(argv)
-    print("this tool is work in progress")
+    pin = args.pin
+    pin_n = int(pin.strip("M"))
+    mode = args.mode
+    current = args.current
+    
+    currents = {
+        '6mA': 0,
+        '3mA': 0,
+    }
+    modes = {
+        'control': 0x00,
+        'status': 0x80,
+    }
+    drv = {
+        'normal': 0,
+        'inverted': 1,
+        'open-drain': 2,
+        'open-drain-inv': 3,
+    }
+    recv = {
+        'and': 0,
+        'nand': 1,
+        'or': 2,
+        'nor': 3,
+    }
+
+    if pin_n < 4:
+        reg = 0x0100    
+    else:
+        reg = 0x0101 
+    
+    r = read_data(handle, address, reg)
+    if args.rcv:
+        write_data(handle, address, reg, r | (recv[args.rcv] << (pin_n*2)))
+    elif args.drv:
+        write_data(handle, address, reg, r | (drv[args.drv] << (pin_n*2)))
+    
+    base = 0x0102
+    r = read_data(handle, address, base + pin_n)
+    write_data(handle, address, base + pin_n, r | modes[args.mode])
+
+    r = read_data(handle, address, 0x0109)
+    write_data(handle, address, 0x0109, r | (currents[args.current] << pin_n))
+    write_data(handle, address, 0x000F, 0x01) # IO update
 
 if __name__ == "__main__":
     main(sys.argv[1:])
