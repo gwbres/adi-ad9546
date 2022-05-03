7cf4f37d44011d3ae3e3bd524b99848db9d0bc12
gwbres <guillaume.bressaix@gmail.com>
2022-05-03 11:17:46 +0200

    working on sysclk script

--- sysclk.py
+++ sysclk.py
@@ -35,14 +35,29 @@ def main (argv):
         ('free-run', None, [], 'Force to free-run state'),
         ('holdover', None, [], 'Force to holdover state'),
         ('fb-div', int, [], 'Set Pll multiplication ratio'),
-        ('freq', float, [], 'Set sys clock reference frequency [Hz]'),
-        0x0202
         ('sel', str, ['direct','crystal'], 'Select ref. frequency source'),
         ('divider', int, [1,2,4,8], 'Set input frequency division ratio (int)'),
         ('doubler', str, ['enable','disable'], 'Enable / Disable input frequency doubler'),
-        0x0201
-        ('stab-period', float, [], 'Set sys clock stability period [s]'),
-        0x0207
+        ('freq', float, [], 'Set sys clock reference frequency [Hz]'),
+        ('stability', float, [], 'Set sys clock ref. stability period [s]'),
+        ('slew-rate-lim', str, [
+            'None',
+            '0.715ppm/s',
+            '1.430ppm/s',
+            '2.860ppm/s',
+            '5.720ppm/s',
+            '11.44ppm/s',
+            '22.88ppm/s',
+            '45.76ppm/s',
+        ], 
+        'Sys clock compensation slew rate limiter control'),
+        ('dpll-source', str, 
+        ['refa','refaa','refb','refbb','aux-ref0','aux-ref1','aux-ref2','aux-ref3'],
+        'Select source for compensation method 3 that uses auxilary Dpll'),
+        ('dpll-bw', float, [],  'Auxilary Dpll Loop filter bandwidth [Hz]'),
+        ('dpll-sel', str, ['dpll0','dpll1'], 'Sys clock compensation, method 2 Dpll channel selection'),
+        ('cutoff', str, ['156Hz','78Hz','39Hz','20Hz','10Hz','5Hz','2Hz','1Hz'], 
+        'Method 1 low pass filter cutoff frequency'),
     ]
     for (v_flag, v_type, v_choices, v_helper) in flags:
         if v_type is None:
@@ -69,6 +84,44 @@ def main (argv):
         'direct': 0,
         'crystal': 1,
     }
+    en = {
+        'disable': 0,
+        'enable': 1,
+    }
+    slew_r = {
+        'None' : 0,
+        '0.715ppm/s': 1,
+        '1.430ppm/s': 2,
+        '2.860ppm/s': 3,
+        '5.720ppm/s': 4,
+        '11.44ppm/s': 5,
+        '22.88ppm/s': 6,
+        '45.76ppm/s': 7,
+    }
+    dpll_sources = {
+        'refa': 0,
+        'refaa': 1,
+        'refb': 2,
+        'refbb': 3,
+        'aux-ref0': 6,
+        'aux-ref1': 7,
+        'aux-ref2': 11,
+        'aux-ref3': 12,
+    }
+    dpll_ch = {
+        'dpll0': 0,
+        'dpll1': 1,
+    }
+    cutoffs = {
+        '156Hz': 0,
+        '78Hz': 1,
+        '39Hz': 2,
+        '20Hz': 3,
+        '10Hz': 4,
+        '5Hz': 5,
+        '2Hz': 6,
+        '1Hz': 7,
+    }
 
     if args.fb_div:
         write_data(handle, address, 0x0200, args.fb_div & 0xFF)
@@ -87,6 +140,37 @@ def main (argv):
         write_data(handle, address, 0x000F, 0x01) # I/O update
         return 0 # force stop
     
+    if args.doubler:
+        r = read_data(handle, address, 0x0201)
+        write_data(handle, address, 0x0201, r | en[args.doubler])
+        write_data(handle, address, 0x000F, 0x01) # I/O update
+        return 0 # force stop
+
+    if args.dpll_source:
+        r = read_data(handle, address, 0x0284)
+        write_data(handle, address, 0x0284, r | dpll_sources[args.dpll_source])
+        write_data(handle, address, 0x000F, 0x01) # I/O update
+        return 0 # force stop
+    
+    if args.dpll_bw:
+        bw = args.dpll_bw * 10
+        write_data(handle, address, 0x0285, r & 0xFF)
+        write_data(handle, address, 0x0285, (r & 0xFF00)>>8)
+        write_data(handle, address, 0x000F, 0x01) # I/O update
+        return 0 # force stop
+
+    if args.dpll_ch:
+        r = read_data(handle, address, 0x0287)
+        write_data(handle, address, 0x0287, r | dpll_ch[args.dpll_ch]) 
+        write_data(handle, address, 0x000F, 0x01) # I/O update
+        return 0 # force stop
+
+    if args.cutoff:
+        r = read_data(handle, address, 0x0288)
+        write_data(handle, address, 0x0288, r | cutoffs[args.cutoff])
+        write_data(handle, address, 0x000F, 0x01) # I/O update
+        return 0 # force stop
+
     if args.free_run:
         reg = read_data(handle, address, 0x2105)
         write_data(handle, address, 0x000F, 0x01) # I/O update
@@ -97,56 +181,30 @@ def main (argv):
         write_data(handle, address, 0x000F, 0x01) # I/O update
         return 0 # force stop
 
-    if args.period:
-        period = round(10E-18/args.period)
-        r0 = period & 0xFF
-        r1 = (period & 0xFF00)>>8
-        r2 = (period & 0xFF0000)>>16
-        r3 = (period & 0xFF000000)>>24
-        r4 = (period & 0xFF00000000)>>32
-        r5 = (period & 0xFF0000000000)>>40
-        r6 = (period & 0xFF000000000000)>>48
-        if pin == 'all':
-            regs = [0x0404, 0x0424, 0x0444, 0x0464, 0x0484, 0x04C4, 0x04E4]
-        elif pin == 'a':
-            regs = [0x0404]
-        elif pin == 'aa':
-            regs = [0x0424]
-        elif pin == 'b':
-            regs = [0x0444]
-        elif pin == 'bb':
-            regs = [0x0464]
-        elif pin == 'aux-0':
-            regs = [0x0484]
-        elif pin == 'aux-1':
-            regs = [0x04A4]
-        elif pin == 'aux-2':
-            regs = [0x04C4]
-        elif pin == 'aux-2':
-            regs = [0x04C4]
-        elif pin == 'aux-3':
-            regs = [0x04E4]
-        for reg in regs:
-            write_data(handle, address, reg+0, r0)
-            write_data(handle, address, reg+1, r1)
-            write_data(handle, address, reg+2, r2)
-            write_data(handle, address, reg+3, r3)
-            write_data(handle, address, reg+4, r4)
-            write_data(handle, address, reg+5, r5)
-            write_data(handle, address, reg+6, r6 & 0xF)
+    if args.freq:
+        freq = args.freq * pow(10,3)
+        write_data(handle, address, 0x0202, freq & 0xFF)
+        write_data(handle, address, 0x0203, (freq & 0xFF00)>>8)
+        write_data(handle, address, 0x0204, (freq & 0xFF0000)>>16)
+        write_data(handle, address, 0x0205, (freq & 0xFF000000)>>24)
+        write_data(handle, address, 0x0206, (freq & 0xFF00000000)>>32)
         write_data(handle, address, 0x000F, 0x01) # I/O update
         return 0
-
-    if args.phase_lock_thresh:
-        value = round(args.phase_lock_thresh * 1E-12)
-        if parser.channel == "all":
-            0x0820 + 0x0821 + 0x0822 # aa
-        else:
- 
-    if args.freq_lock_thresh:
-        df = args.freq_lock_thresh
-        value = round(df / fr * (1/(df+fr)) * pow(10,12))
-            0x0825 + 0x0826 + 0x0827 # aa
+    
+    if args.slew_rate_lim:
+        r = read_data(handle, address, 0x0283)
+        write_data(handle, address, 0x0283, r | slew_r[args.slew_rate_lim])
+        write_data(handle, address, 0x000F, 0x01) # I/O update
+        return 0
+    
+    if args.stability:
+        stab = args.stability * pow(10,3)
+        write_data(handle, address, 0x0207, stab & 0xFF)
+        write_data(handle, address, 0x0208, (stab & 0xFF00)>>8)
+        r = read_data(handle, address, 0x0209)
+        write_data(handle, address, 0x0209, r | ((stab & 0x0F0000)>>16))
+        write_data(handle, address, 0x000F, 0x01) # I/O update
+        return 0
 
 if __name__ == "__main__":
     main(sys.argv[1:])
